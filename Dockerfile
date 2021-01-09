e63a8ccb2e07761927f68a42fb68fb817ece57fc
Wagner Ramos <wagner.ap.ramos@outlook.com>
2021-01-09 15:05:03 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Building Project
+FROM node:alpine as builder
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Creating Production Image
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn /i);
+  expect(linkElement).toBeInTheDocument();
+});
